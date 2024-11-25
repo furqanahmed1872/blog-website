d637f87984bd3251cb2f437b8c15beba230a24b2
Malikhaider2090 <haidermalik2090@gmail.com>
2024-11-25 17:19:15 +0500

    changes in color schemes and screen height issues

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,7 +7,7 @@
 </script>
 
 <!-- Wrapper -->
-<div class="relative h-screen overflow-scroll">
+<div class="relative bg-slate-100 h-screen overflow-hidden">
   <!-- Fixed Background -->
   <div
     class="absolute inset-0 bg-fixed bg-cover bg-center z-0"
@@ -44,90 +44,114 @@
   </div>
 
   <!-- Featured Destinations Section -->
-  <div class="h-full bg-inherit flex flex-col items-center gap-10 w-full">
-    <div class="p-5 px-4 bg-white shadow-2xl w-4/5">
-      <p class="text-2xl font-bold text-black">We travel the world, and tell you how it looks!</p>
-    <p class="text-md font-normal text-black text-center">Proin gravida nibh vel velit auctor aliquet. Aenean sollicitudin, lorem quis bibendum auctor, nisi elit consequat <br> ipsum, nec sagittis sem nibh id elit. Duis sed odio sit amet nibh vulputate cursus a sit amet mauris. Morbi accumsan ipsum velit. <br> Duis sed odio sit amet nibh vulputate cursus a sit amet mauris. Morbi accumsan ipsum velit. <br> Nam nec tellus a odio tincidunt auctor a ornare odio.</p>
-    </div>
-    
-  <div class="bg-white grid grid-cols-3 gap-5 w-4/5">
-    <div class=" col-span-2 flex flex-col gap-5 w-full h-40">
-    <div>
-      <img src="https://www.wildnatureimages.com/images/640/070620-014-The-Tetons.jpg" class="w-full" alt="wanderlast">
-      <p class="text-2xl text-black font-semibold">African Wildlife</p>
-      <p class="text-red-500 text-xl font-bold hover:text-blue-500 cursor-pointer focus:text-yellow-50 visited:text-green-400 ">Africa / Leave a Comment</p>
-      <p class="text-start ">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo. Sed non mauris vitae erat consequat auctor </p>
-          
-    </div>
-    <div class="grid grid-cols-2 gap-5 ">
-      <div class="bg-slate-200">
-        <img src="https://plus.unsplash.com/premium_photo-1672115680958-54438df0ab82?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8bW91bnRhaW5zfGVufDB8fDB8fHww" class="w-full" alt="wanderlast">
-       <div class="p-5">
-        <p class="text-2xl text-black font-semibold">A day on the mountains</p>  
-        <p class="text-red-500 text-xl font-bold hover:text-blue-500 cursor-pointer focus:text-yellow-50 visited:text-green-400">Asia / Leave a Comment</p>
-        <p  class="text-start">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo. Sed non mauris vitae erat consequat auctor A day on the mountainsRead More »</p>                                                                                                                                                                                                           
-
-       </div>
-        
-      </div>
-
-      <div class="bg-slate-200">
-        <img src="https://plus.unsplash.com/premium_photo-1672115680958-54438df0ab82?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8bW91bnRhaW5zfGVufDB8fDB8fHww" class="w-full" alt="wanderlast">
-       <div class="p-5">
-        <p class="text-2xl text-black font-semibold">A day on the mountains</p>  
-        <p class="text-red-500 text-xl font-bold hover:text-blue-500 cursor-pointer focus:text-yellow-50 visited:text-green-400">Asia / Leave a Comment</p>
-        <p  class="text-start">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo. Sed non mauris vitae erat consequat auctor A day on the mountainsRead More »</p>                                                                                                                                                                                                           
-
-       </div>
-        
-      </div>
-
-      <div class="bg-slate-200">
-        <img src="https://plus.unsplash.com/premium_photo-1672115680958-54438df0ab82?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8bW91bnRhaW5zfGVufDB8fDB8fHww" class="w-full" alt="wanderlast">
-       <div class="p-5">
-        <p class="text-2xl text-black font-semibold">A day on the mountains</p>  
-        <p class="text-red-500 text-xl font-bold hover:text-blue-500 cursor-pointer focus:text-yellow-50 visited:text-green-400">Asia / Leave a Comment</p>
-        <p  class="text-start">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo. Sed non mauris vitae erat consequat auctor A day on the mountainsRead More »</p>                                                                                                                                                                                                           
-
-       </div>
-        
-      </div>
-
-      <div class="bg-slate-200">
-        <img src="https://plus.unsplash.com/premium_photo-1672115680958-54438df0ab82?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8bW91bnRhaW5zfGVufDB8fDB8fHww" class="w-full" alt="wanderlast">
-       <div class="p-5">
-        <p class="text-2xl text-black font-semibold">A day on the mountains</p>  
-        <p class="text-red-500 text-xl font-bold hover:text-blue-500 cursor-pointer focus:text-yellow-50 visited:text-green-400">Asia / Leave a Comment</p>
-        <p  class="text-start">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo. Sed non mauris vitae erat consequat auctor A day on the mountainsRead More »</p>                                                                                                                                                                                                           
-
-       </div>
-        
-      </div>
-
-      <div class="bg-slate-200">
-        <img src="https://plus.unsplash.com/premium_photo-1672115680958-54438df0ab82?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8bW91bnRhaW5zfGVufDB8fDB8fHww" class="w-full" alt="wanderlast">
-       <div class="p-5">
-        <p class="text-2xl text-black font-semibold">A day on the mountains</p>  
-        <p class="text-red-500 text-xl font-bold hover:text-blue-500 cursor-pointer focus:text-yellow-50 visited:text-green-400">Asia / Leave a Comment</p>
-        <p  class="text-start">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo. Sed non mauris vitae erat consequat auctor A day on the mountainsRead More »</p>                                                                                                                                                                                                           
-
-       </div>
-        
+  
+  <div class=" w-full relative h-full">
+    <div class="h-full absolute -top-20 flex flex-col items-center gap-10 w-full">
+      <div class="p-5 px-4 bg-white shadow-2xl flex flex-col items-center w-4/5">
+        <p class="text-2xl font-bold text-black ">We travel the world, and tell you how it looks!</p>
+      <p class="text-md font-normal text-black text-center">Proin gravida nibh vel velit auctor aliquet. Aenean sollicitudin, lorem quis bibendum auctor, nisi elit consequat <br> ipsum, nec sagittis sem nibh id elit. Duis sed odio sit amet nibh vulputate cursus a sit amet mauris. Morbi accumsan ipsum velit. <br> Duis sed odio sit amet nibh vulputate cursus a sit amet mauris. Morbi accumsan ipsum velit. <br> Nam nec tellus a odio tincidunt auctor a ornare odio.</p>
       </div>
       
+    <div class="bg-inherit grid grid-cols-3 gap-5 w-4/5">
+      <div class=" col-span-2 flex flex-col gap-5 w-full h-40">
+      <div class="bg-slate-200">
+        <img src="https://www.wildnatureimages.com/images/640/070620-014-The-Tetons.jpg" class="w-full" alt="wanderlast">
+        <p class="text-2xl text-black font-semibold">African Wildlife</p>
+        <p class="text-red-500 text-xl font-bold hover:text-blue-500 cursor-pointer focus:text-yellow-50 visited:text-green-400 ">Africa / Leave a Comment</p>
+        <p class="text-start ">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo. Sed non mauris vitae erat consequat auctor </p>
+            
+      </div>
+      <div class="grid grid-cols-2 gap-5 ">
+        <div class="bg-slate-200">
+          <img src="https://plus.unsplash.com/premium_photo-1672115680958-54438df0ab82?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8bW91bnRhaW5zfGVufDB8fDB8fHww" class="w-full" alt="wanderlast">
+         <div class="p-5">
+          <p class="text-2xl text-black font-semibold">A day on the mountains</p>  
+          <p class="text-red-500 text-xl font-bold hover:text-blue-500 cursor-pointer focus:text-yellow-50 visited:text-green-400">Asia / Leave a Comment</p>
+          <p  class="text-start">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo. Sed non mauris vitae erat consequat auctor A day on the mountainsRead More »</p>                                                                                                                                                                                                           
+  
+         </div>
+          
+        </div>
+  
+        <div class="bg-slate-200">
+          <img src="https://plus.unsplash.com/premium_photo-1672115680958-54438df0ab82?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8bW91bnRhaW5zfGVufDB8fDB8fHww" class="w-full" alt="wanderlast">
+         <div class="p-5">
+          <p class="text-2xl text-black font-semibold">A day on the mountains</p>  
+          <p class="text-red-500 text-xl font-bold hover:text-blue-500 cursor-pointer focus:text-yellow-50 visited:text-green-400">Asia / Leave a Comment</p>
+          <p  class="text-start">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo. Sed non mauris vitae erat consequat auctor A day on the mountainsRead More »</p>                                                                                                                                                                                                           
+  
+         </div>
+          
+        </div>
+  
+        <div class="bg-slate-200">
+          <img src="https://plus.unsplash.com/premium_photo-1672115680958-54438df0ab82?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8bW91bnRhaW5zfGVufDB8fDB8fHww" class="w-full" alt="wanderlast">
+         <div class="p-5">
+          <p class="text-2xl text-black font-semibold">A day on the mountains</p>  
+          <p class="text-red-500 text-xl font-bold hover:text-blue-500 cursor-pointer focus:text-yellow-50 visited:text-green-400">Asia / Leave a Comment</p>
+          <p  class="text-start">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo. Sed non mauris vitae erat consequat auctor A day on the mountainsRead More »</p>                                                                                                                                                                                                           
+  
+         </div>
+          
+        </div>
+  
+        <div class="bg-slate-200">
+          <img src="https://plus.unsplash.com/premium_photo-1672115680958-54438df0ab82?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8bW91bnRhaW5zfGVufDB8fDB8fHww" class="w-full" alt="wanderlast">
+         <div class="p-5">
+          <p class="text-2xl text-black font-semibold">A day on the mountains</p>  
+          <p class="text-red-500 text-xl font-bold hover:text-blue-500 cursor-pointer focus:text-yellow-50 visited:text-green-400">Asia / Leave a Comment</p>
+          <p  class="text-start">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo. Sed non mauris vitae erat consequat auctor A day on the mountainsRead More »</p>                                                                                                                                                                                                           
+  
+         </div>
+          
+        </div>
+  
+        <div class="bg-slate-200">
+          <img src="https://plus.unsplash.com/premium_photo-1672115680958-54438df0ab82?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8bW91bnRhaW5zfGVufDB8fDB8fHww" class="w-full" alt="wanderlast">
+         <div class="p-5">
+          <p class="text-2xl text-black font-semibold">A day on the mountains</p>  
+          <p class="text-red-500 text-xl font-bold hover:text-blue-500 cursor-pointer focus:text-yellow-50 visited:text-green-400">Asia / Leave a Comment</p>
+          <p  class="text-start">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo. Sed non mauris vitae erat consequat auctor A day on the mountainsRead More »</p>                                                                                                                                                                                                           
+  
+         </div>
+          
+        </div>
+        
+      </div>
+  
+     </div>
+      <div class= "flex flex-col col-span-1 gap-6 w-full h-full ">
+        <div class="p-6 bg-white">
+          
+            
+            <input type="search" id="gsearch" name="gsearch" placeholder="search" class="p-2 w-full border border-gray-300 rounded-sm focus:outline-none focus:ring focus:ring-blue-300 hover:bg-orange-200" >
+            
+        </div>
+        <div class="p-6 bg-white">
+          <p>Recent Photos</p>
+          <p>1</p>
+          <p>2</p>
+          <p>3</p>
+        </div>
+        <div class="p-6 bg-white">
+          <p>Categories</p>
+          <p>1</p>
+          <p>2</p>
+          <p>3</p>
+        </div>
+        <div class="p-6 bg-white">
+          <p>Archives</p>
+          <p>1</p>
+          <p>2</p>
+          <p>3</p>
+        </div>
+  
+      </div>
+  
+  
     </div>
-
-   </div>
-    <div class="bg-neutral-600 flex flex-col col-span-1 w-full h-full ">
-
+  
     </div>
-
-
-  </div>
-
-  </div>
-  <div class="bg-white w-full h-40">
-    
   </div>
   <!-- About Section
    <!-- Blog Section -->
@@ -136,12 +160,12 @@
 
 
   <!-- Footer -->
-  <!-- <footer class="bg-blue-500 text-white bottom-0 py-4">
-    <div class="container mx-auto text-center">
-      <p>&copy; 2024 Wanderlust. All rights reserved.</p>
-    </div>
-  </footer> -->
 </div>
+<footer class="bg-blue-500 text-white py-4">
+  <div class="mx-auto text-center">
+    <p>&copy; 2024 Wanderlust. All rights reserved.</p>
+  </div>
+</footer>
 
 
 
