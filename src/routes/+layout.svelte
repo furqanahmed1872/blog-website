6b972c5ccadde345cee17d2420c5a127d06ec296
Dev <98402746+furqanahmed1872@users.noreply.github.com>
2024-11-27 16:34:49 +0500

    ok

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,13 @@
 <script>
-    import "../app.css";
-  </script>
-  
+  import "../app.css";
+</script>
+
+<div class="w-full flex flex-col mx-auto">
   <slot />
+
+  <div class="bg-blue-500 mt-6 text-white py-4">
+    <div class="mx-auto text-center">
+      <p>&copy; 2024 Wanderlust. All rights reserved.</p>
+    </div>
+  </div>
+</div>
