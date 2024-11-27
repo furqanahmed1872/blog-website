ea866d9bc0147708e0f3e9636179547514e5b06a
Malikhaider2090 <haidermalik2090@gmail.com>
2024-11-27 17:30:09 +0500

    article page

---
+++ src/routes/test/+page.svelte
@@ -0,0 +1,30 @@
+<div class="bg-white flex flex-col gap-5 w-full items-center p-5 ">
+    <!-- First Section -->
+    <div class="bg-slate-100 p-4 rounded shadow-lg flex flex-col gap-2">
+      <h2 class="text-xl font-bold text-gray-800">Section 1</h2>
+      <img src= "/pic1.jpg" alt="" class="relative">
+      <p class="text-sm text-gray-600"> <span class="hover:cursor-pointer">Leave a Comment / Africa / By harshadd </span> <br>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo. Sed non mauris vitae erat consequat auctor eu in elit.Class aptent taciti sociosqu ad litora torquent per conubia nostra. Etiam pharetra, erat sed fermentum feugiat, velit mauris egestas quam, ut aliquam massa nisl quis neque. Suspendisse in orci enim. Mauris in erat justo. Nullam ac urna eu felis dapibus condimentum sit amet a augue. Nullam ac urna eu felis dapibus condimentum sit amet a augue. Sed non neque elit. Sed ut imperdiet nisi. Proin condimentum fermentum nunc.
+        
+        Etiam pharetra, erat sed fermentum feugiat, velit mauris egestas quam, ut aliquam massa nisl quis neque. Suspendisse in orci enim. Mauris in erat justo. Nullam ac urna eu felis dapibus condimentum sit amet a augue.
+        
+        Proin gravida nibh vel velit auctor aliquet. Aenean sollicitudin, lorem quis bibendum auctor, nisi elit consequat ipsum, nec sagittis sem nibh id elit. Duis sed odio sit amet nibh vulputate cursus a sit amet mauris. Morbi accumsan ipsum velit. Duis sed odio sit amet nibh vulputate cursus a sit amet mauris. Morbi accumsan ipsum velit. Nam nec tellus a odio tincidunt auctor a ornare odio.
+        
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo. Sed non mauris vitae erat consequat auctor eu in elit. Class aptent taciti sociosqu ad litora torquent per conubia nostra.
+        Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo.</p>
+    </div>
+  
+    <!-- Second Section -->
+    <div class="bg-white p-4 rounded shadow-md">
+      <h2 class="text-xl font-bold text-gray-800">Section 2</h2>
+      <p class="text-sm text-gray-600">This is the second section. Include your details here.</p>
+    </div>
+  
+    <!-- Third Section -->
+    <div class="bg-white p-4 rounded shadow-md">
+      <h2 class="text-xl font-bold text-gray-800">Section 3</h2>
+      <p class="text-sm text-gray-600">This is the third section. Add additional info or links here.</p>
+    </div>
+  
+  
+</div>
