217fb6fc5a1ef243aead99343bddc1d2874e339c
Malikhaider2090 <haidermalik2090@gmail.com>
2024-11-28 17:52:19 +0500

    changes in footer

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,13 +4,46 @@
 
 <div class="w-full flex flex-col mx-auto">
   <slot />
+<div class="bg-black flex flex-col mt-6 p-8">
 
-  <div class="bg-blue-500 mt-6 text-white py-4">
-    <div class="mx-auto text-center">
-      <img width="160" height="84" src="https://websitedemos.net/wanderlust-travel-diary-04/wp-content/uploads/sites/154/2018/01/logo-white-free-img-1-160x84.png" class="image wp-image-2069  attachment-160x84 size-160x84" alt="" style="max-width: 100%; height: auto;" decoding="async" srcset="" sizes="(max-width: 160px) 100vw, 160px">
-      <p>&copy; 2024 Wanderlust. All rights reserved.</p>
+  <div class="bg-black  text-white py-4 grid grid-cols-4 grid-flow-col">
+    <div class="flex flex-col">
+      <img width="150" height="100" src="https://websitedemos.net/wanderlust-travel-diary-04/wp-content/uploads/sites/154/2018/01/logo-white-free-img-1-160x84.png" class="image wp-image-2069  attachment-160x84 size-160x84" alt="" style="max-width: 100%; height: auto;" decoding="async" srcset="" sizes="(max-width: 160px) 100vw, 160px">
+      <!-- <p>&copy; 2024 Wanderlust. All rights reserved.</p> -->
+       <p>Nullam ac urna eu felis dapibus condimentum sit amet a augue. Sed non neque elit. Sed ut imperdiet nisi.</p>
     </div>
+
+    <div class="flex flex-col">
+    Catogeries 
+    <p>1</p>
+     <p>2</p>
+     <p>3</p>
+     <p>4</p>
+     <p>5</p>
+    </div>
+
+    <div class="flex flex-col">
+     Recent Photos
+     <p>1</p>
+     <p>2</p>
+     <p>3</p>
+     <p>4</p>
+     <p>5</p>
+
+    </div>
+
+    <div class="flex flex-col">
+     Gallery
+    </div>
+   
   </div>
+  <div class="text-white flex flex-row justify-between">
+  <p >&copy; 2024 Wanderlust. All rights reserved.</p>
+  <p>Powered by Wanderlust Travel Diary</p>
+</div>
+
+</div>
+
 </div>
 
 
